e472ae1f2f0e811cc7959b35471eb6b1a45b3344
Peyam Maroufi <maroufi@kth.se>
2021-01-04 23:15:04 +0100

    refactor: removing all unwanted files and refactored some files

--- youtubeDownloader/YoutubeDownloader.py
+++ youtubeDownloader/YoutubeDownloader.py
@@ -1,119 +1,90 @@
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 # Copyright 2019 Payam Maroufi
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
+import sys
+import Downloader
+from gi.repository import Gtk, Gio, Gdk
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, Gio, Gdk
-import Downloader
-import sys
 
 
 # lists of rbbttons
 lst_rbVideo = []
 lst_rbMusic = []
 
+
 class youtubeD(Gtk.ApplicationWindow):
 
     def __init__(self, app):
-        Gtk.Window.__init__(self, title="Youtube Download GTK", application=app)
-        self.set_border_width(10)
+        Gtk.Window.__init__(self, title="Youtube Downloader", application=app)
+        self.set_border_width(5)
         self.set_size_request(580, 500)
-
-        # HEADERBAR
-        hb = Gtk.HeaderBar()
-        hb.set_show_close_button(True)
-        hb.props.title = "Youtube Downloader"
-        self.set_titlebar(hb)
-
-        btnSetting = Gtk.Button()
-        icon = Gio.ThemedIcon(name="applications-system")
-        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
-        btnSetting.add(image)
-        btnSetting.connect("clicked", self.on_btnSetting_clicked)
-        hb.pack_end(btnSetting)
-        # END HEADERBAR
-
-        # The main
-        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=5)
+        main_box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=3)
         self.add(main_box)
 
-        # ## UPPER BOX
-        first_box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
-        first_box.set_border_width(4)
+        """ Setting header. """
+        __hb = Gtk.HeaderBar()
+        __hb.set_show_close_button(True)
+        __hb.props.title = "Youtube Downloader"
+        __setting_btn = Gtk.Button()
+        __setting_btn.add(Gtk.Image.new_from_gicon(Gio.ThemedIcon(
+            name="applications-system"), Gtk.IconSize.BUTTON))
+        __hb.pack_end(__setting_btn)
+        self.set_titlebar(__hb)
 
-        # ## Button with image
-        hbox = Gtk.HBox()
-        hbox.set_halign(Gtk.Align.CENTER)
+        """ Adding download and output type box. """
+        __hbox_setting = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
+        __hbox_setting.set_border_width(3)
 
-        add_button = Gtk.Button()
-        plus_icon = Gio.ThemedIcon(name="list-add")
-        image = Gtk.Image.new_from_gicon(plus_icon, Gtk.IconSize.BUTTON)
-        add_button.add(image)
-        # ## END Button with label
+        # Button in the setting bar
+        __addDown_btn = Gtk.Button()
+        __addDown_btn.add(Gtk.Image.new_from_gicon(
+            Gio.ThemedIcon(name="list-add"), Gtk.IconSize.BUTTON))
 
-        # ## Label
-        newLabel = Gtk.Label()
-        newLabel.set_markup("<span font='Ubuntu 9' font_weight='normal'>Add new download</span>")
-        # ## END Label
+        # Download label
+        __addDown_lbl = Gtk.Label()
+        __addDown_lbl.set_markup(
+            "<span font='Ubuntu 9' font_weight='normal'>Add new download</span>")
 
-        # ## Label with icon and Combobox
-        """
-         Image
-         label
-         Combobox
-         """
-        lbWithIcon = Gtk.HBox()
-        lbWithIcon.set_halign(Gtk.Align.END)
+        # Output type label
+        __outputType_lbl = Gtk.Label()
+        __outputType_lbl.set_markup(
+            "<span font='Ubuntu 9' font_weight='normal'> Output type: </span>")
 
-        Yimage = Gtk.Image()
-        Yimage.set_from_icon_name("applications-multimedia", 4)
-
-        lbOutputType = Gtk.Label()
-        lbOutputType.set_markup("<span font='Ubuntu 9' font_weight='normal'> Output type: </span>")
-
-        cbOutput = Gtk.ComboBoxText()
+        # Output type combo box
+        __output_cb = Gtk.ComboBoxText()
         output_vector = ["Custom", "Video", "Audio"]
         for output in output_vector:
-            cbOutput.append_text(output)
+            __output_cb.append_text(output)
+        __output_cb.set_entry_text_column(0)
 
-        cbOutput.set_entry_text_column(0)
+        # Add everything to the download and output box
+        __hbox_setting.pack_start(__addDown_btn, False, False, 0)
+        __hbox_setting.pack_start(__addDown_lbl, False, False, 4)
+        __hbox_setting.pack_end(__output_cb, False, False, 4)
+        __hbox_setting.pack_end(__outputType_lbl, False, False, 4)
 
-        # ## END Label with icon and Combobox
+        # Add the download and output box to the main box of the window
+        main_box.pack_start(__hbox_setting, False, True, 0)
 
-        # ## ADDING TO LAYOUT
-        hbox.pack_start(add_button, False, False, 0)
-        hbox.pack_start(newLabel, False, False, 4)
-        lbWithIcon.pack_start(Yimage, False, False, 0)
-        lbWithIcon.pack_start(lbOutputType, False, False, 0)
-        lbWithIcon.pack_start(cbOutput, False, False, 4)
-        first_box.pack_start(hbox, False, False, 0)
-        first_box.pack_start(lbWithIcon, True, True, 0)
-        main_box.pack_start(first_box, False, True, 0)
-        # ## END ADDING TO LAYOUT
-
-        # ## EVENT FUNCTIONS FOR COMBOBOX AND BUTTON
-        cbOutput.connect("changed", self.on_cbOuput_changed)
-        add_button.connect("clicked", self.on_btnAdd_click)
-        # ## END EVENT FUNCTIONS FOR COMBOBOX AND BUTTON
-        # # END UPPER BOX
-
+        """ Connecting each functional controller to its function """
+        __setting_btn.connect("clicked", self.on_btnSetting_clicked)
+        __output_cb.connect("changed", self.on_cbOuput_changed)
+        __addDown_btn.connect("clicked", self.on_btnAdd_click)
 
         # ## THE DOWN BOX CONSISTING OF A LISTBOX
         self.downBox = Gtk.ListBox()
         self.downBox.set_selection_mode(Gtk.SelectionMode.NONE)
 
-
         self.win_scroll = Gtk.ScrolledWindow()
         self.win_scroll.add(self.downBox)
-        self.win_scroll.set_policy(Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
+        self.win_scroll.set_policy(
+            Gtk.PolicyType.NEVER, Gtk.PolicyType.AUTOMATIC)
         self.win_scroll.set_min_content_height(100)
         main_box.pack_start(self.win_scroll, True, True, 0)
-        # ## END ADDING TO LAYOUT
-        # # END THE DOWN BOX CONSISTING OF A LISTBOX
-        # END of ALL
 
     # ## METHODS
 
@@ -144,12 +115,10 @@ class youtubeD(Gtk.ApplicationWindow):
         progress_bar = Gtk.ProgressBar()
         progress_bar.set_margin_bottom(2)
         progress_bar.set_margin_top(3)
-        progress_bar.modify_bg(Gtk.StateType.NORMAL, Gdk.color_parse("#58ACFA"))
-
+        progress_bar.modify_bg(Gtk.StateType.NORMAL,
+                               Gdk.color_parse("#58ACFA"))
 
         self.quality_hbox = Gtk.HBox()
-
-
 
         radiobtn_box = Gtk.VBox()
         radiobtn_box.set_valign(Gtk.Align.CENTER)
@@ -158,12 +127,14 @@ class youtubeD(Gtk.ApplicationWindow):
         rbAudio = Gtk.RadioButton.new_from_widget(rbVideo)
         rbAudio.set_label("Audio")
 
-        link_entry.connect("activate", lambda widget: self.entry_text_changed(link_entry, rbVideo, rbAudio))
+        link_entry.connect("activate", lambda widget: self.entry_text_changed(
+            link_entry, rbVideo, rbAudio))
         btnDownload = Gtk.Button()
         download_icon = Gio.ThemedIcon(name="down")
         image = Gtk.Image.new_from_gicon(download_icon, Gtk.IconSize.BUTTON)
         btnDownload.add(image)
-        btnDownload.connect("clicked", lambda widget: self.on_btnDownload_click(link_entry, rbVideo, rbAudio))
+        btnDownload.connect("clicked", lambda widget: self.on_btnDownload_click(
+            link_entry, rbVideo, rbAudio))
         lst_rbVideo.append(rbVideo)
         lst_rbMusic.append(rbAudio)
 
@@ -179,10 +150,9 @@ class youtubeD(Gtk.ApplicationWindow):
         self.row.add(downBox_main)
         self.downBox.add(self.row)
         self.downBox.show_all()
-        self.selected_qualities =[]
+        self.selected_qualities = []
         self.selected_format = []
         self.y = False
-
 
     def on_cbOuput_changed(self, combo):
         # is used when the combobox changes its member
@@ -213,14 +183,15 @@ class youtubeD(Gtk.ApplicationWindow):
 
             print(self.download_Url)
 
-            self.title_quality, self.y = Downloader.YouTubeDLR.get_information(self, self.download_Url)
+            self.title_quality, self.y = Downloader.YouTubeDLR.get_information(
+                self, self.download_Url)
             if self.y:
                 if rbVideo.get_active():
                     link_entry.set_text(self.title_quality[2])
                     link_entry.set_editable(False)
                     print(self.title_quality[1])
                     self.available_format_code = self.title_quality[0]
-    
+
                     # Creating toggle buttons for each quality
                     x = []
                     for buttonNames in self.title_quality[1]:
@@ -228,14 +199,14 @@ class youtubeD(Gtk.ApplicationWindow):
                         btn.set_label(buttonNames)
                         x.append(btn)
                         self.quality_hbox.pack_start(btn, True, True, 2)
-                        btn.connect("toggled", lambda  x: self.on_button_toggled(x))
-    
+                        btn.connect(
+                            "toggled", lambda x: self.on_button_toggled(x))
+
                         self.quality_hbox.show_all()
                 else:
                     link_entry.set_text(self.title_quality[2])
         else:
             link_entry.set_text("Something went wrong")
-                        
 
     def on_btnDownload_click(self, link_entry, rbVideo, rbAudio):
         # ## Without pressing enter no return value will be read. 'y' is set to false
@@ -245,7 +216,8 @@ class youtubeD(Gtk.ApplicationWindow):
         if self.y:
             if link_entry.get_text() != "" and rbVideo.get_active():
                 if len(self.selected_qualities) != 0:
-                    Downloader.YouTubeDLR.get_video(self, self.download_Url, self.selected_format, self.selected_qualities)
+                    Downloader.YouTubeDLR.get_video(
+                        self, self.download_Url, self.selected_format, self.selected_qualities)
 
             elif link_entry.get_text() != "" and rbAudio.get_active():
                 print(self.download_Url)
@@ -261,18 +233,17 @@ class youtubeD(Gtk.ApplicationWindow):
                 # Choosing the toggled options
                 self.selected_qualities.append(selected_Tbtn)
                 # Choosing the corresponding format code
-                self.selected_format.append(self.available_format_code[self.title_quality[1].index(selected_Tbtn)])
+                self.selected_format.append(
+                    self.available_format_code[self.title_quality[1].index(selected_Tbtn)])
                 print(self.selected_qualities)
                 print(self.selected_format)
         else:
             self.selected_qualities.remove(selected_Tbtn)
-            self.selected_format.remove(self.available_format_code[self.title_quality[1].index(selected_Tbtn)])
+            self.selected_format.remove(
+                self.available_format_code[self.title_quality[1].index(selected_Tbtn)])
             print(self.selected_qualities)
             print(self.selected_format)
             print(len(self.selected_qualities))
-
-
-
 
     def on_btnSetting_clicked(self, button):
         # The form is ready
@@ -293,6 +264,7 @@ class MyApplication(Gtk.Application):
 
     def do_startup(self):
         Gtk.Application.do_startup(self)
+
 
 app = MyApplication()
 exit_status = app.run(sys.argv)
